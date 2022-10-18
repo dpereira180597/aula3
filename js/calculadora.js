c1e2e3d5587ad04b59972c6524bdca4ab2e5c49e
Daniel Pereira <daniel.pereira@oneshop.pt>
2022-10-18 20:12:43 +0100

    projecto 3

---
+++ js/calculadora.js
@@ -0,0 +1,40 @@
+flag = 0;
+
+
+function limpa(){
+    // decumento actual + name do form + name do input de dados + value para obter o valor
+    document.form1.visor.value =" ";
+
+}
+
+
+function valor(v){
+    if(flag == 1){
+        if(v == '/'|| v == '*'|| v == '-'|| v == '+'){
+            var atual = document.form1.visor.value;
+            document.form1.visor.value = atual+v;
+            flag = 0;
+        }else{
+            limpa();
+            flag = 0;
+            var atual = document.form1.visor.value;
+            document.form1.visor.value = atual+v;
+        }
+        }else{
+            var atual = document.form1.visor.value;
+            document.form1.visor.value = atual+v;
+        
+    }
+}
+
+
+
+function calcula(){
+
+
+var resultado =eval(document.form1.visor.value);
+
+document.form1.visor.value = resultado;
+flag = 1;
+
+}
